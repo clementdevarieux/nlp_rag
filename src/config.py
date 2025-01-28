b32df217100ab09828a395c39319fac51face100
Arys <charles.yaiche@epita.fr>
2025-01-28 09:49:13 +0100

    add a config file with every path

---
+++ src/config.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+from dotenv import load_dotenv
+from loguru import logger
+
+load_dotenv()
+
+PROJ_ROOT = Path(__file__).resolve().parents[1]
+logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
+
+
+DATA_DIR = PROJ_ROOT / 'data'
+DOCUMENTS_DIR = DATA_DIR / 'documentation/rust'
+
