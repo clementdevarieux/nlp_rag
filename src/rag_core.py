9c63647c034f9b3988cacd705ba95ff5c3beab82
Arys <charles.yaiche@epita.fr>
2025-02-10 10:26:34 +0100

    add tfidf retriever

--- src/rag_core.py
+++ src/rag_core.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 from src.config import DOCUMENTS_DIR
 from src.llm_query import call_llm
-from src.retriever import return_response
+from src.retriever import retreive_response, get_fittest_chunk, tfidf_vectorizer
 
 path = Path(DOCUMENTS_DIR)
 
@@ -12,19 +12,27 @@ for filename in path.glob("*.md"):
         texts.append(f.read())
 
 # %%
-user_query: str = "how do do the sum of two  tensor"
+user_query: str = "how to sum two tensor"
 
 
 def parse_class_add_title(text):
     chunks = text.split("##")
-    title = chunks[0].split("#")[0]
-    return {"title": title, "chunks": [f"{title}|||: {chunk}" for chunk in chunks]}
+    title = chunks[0].split("\n")[0]
+    return [
+        {"title": title, 'chunk': f"{title}|||: {chunk}"} for chunk in chunks]
 
-chunks = sum((parse_class_add_title(txt)["chunks"] for txt in texts), [])
 
-relevent_document = return_response(chunks, user_query)
+structured_chunks = [parse_class_add_title(txt) for txt in texts]
+chunks = [chunk_value for chunk in structured_chunks for chunk_value in chunk]
+
+fittest_document_id = retreive_response(chunks, user_query)
+#using jaccard_similarity
+# fittest_document = get_fittest_chunk(relevent_documents, user_query)
+fittest_document_id = tfidf_vectorizer(chunks, user_query)
+
+fittest_document = chunks[fittest_document_id]
 
 full_response = []
 
-response = call_llm(user_query, relevent_document['response'], llm="llama2")
+response = call_llm(user_query, fittest_document['response'], llm="llama2")
 print(response)
--- src/retriever.py
+++ src/retriever.py
@@ -1,3 +1,11 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+from transformers import AutoTokenizer, AutoModel
+from transformers import BertTokenizer, BertModel
+import torch
+import torch.nn.functional as F
+
 def jaccard_similarity(user_query, chunk):
     user_query = user_query.lower().split(" ")
     chunk = chunk.lower().split(" ")
@@ -6,15 +14,43 @@ def jaccard_similarity(user_query, chunk):
     return len(intersection) / len(union)
 
 
-def return_response(chunks: list, user_query: str) -> dict:
+def retreive_response(chunks_struct: list, user_query: str) -> dict:
     similarities = []
-    for chunk in chunks:
-        similarity = jaccard_similarity(user_query, chunk)
-        similarities.append(similarity)
+    for i, chunk in enumerate(chunks_struct):
+        similarity = jaccard_similarity(user_query, chunk['chunk'])
+        similarities.append((i, similarity, chunk))
 
-    fittest_chunk = chunks[similarities.index(max(similarities))]
+    ordered_similarities = sorted(similarities, key=lambda x: x[1], reverse=True)
+    return ordered_similarities[0][0]
+
+def get_fittest_chunk(ordered_similarities: list, user_query:str) -> dict:
+    fittest_chunk = ordered_similarities[0][2]
     return {
         "user_query": user_query,
-        "chunk_name": fittest_chunk.split("|||")[0],
+        "chunk_name": fittest_chunk['title'],
         "response": fittest_chunk,
     }
+
+
+def tfidf_vectorizer(chunks_struct: list, user_query: str):
+    txt_chunk = []
+    for chunk in chunks_struct:
+        txt_chunk.append(chunk['chunk'])
+    vectorizer = TfidfVectorizer()
+    tfidf_matrix = vectorizer.fit_transform(txt_chunk + [user_query])
+
+    cosin_similarities = cosine_similarity(tfidf_matrix[-1], tfidf_matrix[:-1])
+    fittest_chunk_index = np.argmax(cosin_similarities)
+    return fittest_chunk_index
+
+def transformers_retriever(chunks_struct: list, user_query: str):
+    tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
+    model = BertModel.from_pretrained('bert-base-cased')
+
+    def encode_text(texts, tokenizer, model):
+        inputs = tokenizer(texts, padding=True, truncation=True, return_tensors='pt', max_length=512)
+        with torch.no_grad():
+            outputs = model(**inputs)
+        embeddings = outputs.last_hidden_state[:, 0, :]
+        return embeddings
+    #TODO
